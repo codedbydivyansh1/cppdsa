fd9d9960ee0a84c05906472bb17182618ed3e9f8
divyansh shukla <divyanshshukla3107@gmail.com>
2025-10-19 19:23:02 +0530

    done

---
+++ first.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+// int main()
+// {
+//     cout<<"hello coder army";
+// }
+
+int main(){
+    // variable declare 4bytes 
+    // int a =20,b=20;
+    // cout<<a+b;
+    
+    // char c = 'a'; 1byte
+    // cout<<c; 
+
+
+    // float f = 2.32;
+    // cout<<f;
+
+    // double d = 3.1231221;
+    // cout<<d;
+
+    bool b = false;
+    cout<<b;
+
+    
+}
